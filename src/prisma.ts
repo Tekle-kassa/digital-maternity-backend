058e32db05d14d8fb107811b33807bd6833db23c
Teklee <kassateklee@gmail.com>
2025-11-11 12:25:16 +0300

    feat: initialize digital maternity backend with TypeScript, Express, and Prisma

---
+++ src/prisma.ts
@@ -0,0 +1,7 @@
+// src/prisma.ts
+// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
+
+const prisma = new PrismaClient();
+
+export default prisma;
