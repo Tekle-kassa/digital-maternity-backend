7cdaa750def044bd8d28bb1d4b956dadc5fcbe8f
Teklee <kassateklee@gmail.com>
2025-12-11 10:01:21 +0300

    delete dist

--- dist/config/prisma.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/prisma.ts
-// import { PrismaClient } from "@prisma/client";
-// import { PrismaClient } from "../generated/prisma/client";
-const client_1 = require("../generated/prisma/client");
-const prisma = new client_1.PrismaClient();
-exports.default = prisma;
